e942a2223567553cccdaee85ec5383d63f59b21d
jaynson <lyghtshades@outlook.com>
2022-05-14 08:32:29 -0800

    Updated UI with member cards and details page

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  styleUrls: ['./member-list.component.css'],
 })
-export class MemberListComponent implements OnInit {
+export class MemberListComponent implements OnInit, OnDestroy {
+  members: Member[];
+  membersSubcription: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private memberService: MembersService) {}
+  ngOnDestroy(): void {
+    this.membersSubcription?.unsubscribe();
   }
 
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.membersSubcription = this.memberService.getMembers().subscribe({
+      next: (loadedMembers) => (this.members = loadedMembers),
+    });
+  }
 }
